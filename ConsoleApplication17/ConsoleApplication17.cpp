996c11f197f92e909c542d831220696a4281ab71
obsidian909@gmail.com <obsidian909@gmail.com>
2021-01-29 20:09:22 +0400

    Задача с Lрешение задачи на L

--- ConsoleApplication17/ConsoleApplication17.cpp
+++ ConsoleApplication17/ConsoleApplication17.cpp
@@ -2,9 +2,38 @@
 
 using namespace std;
 
+
 int main()
 {
     setlocale(LC_ALL, "ru");
+    int array[10];
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Введите " << i + 1 << "й элемент массива\n";
+        cin >> array[i];
+        system("cls");
+    }
+    {
+        cout << "{";
+        for (int i = 0; i < 10; i++)
+        {
+            cout << array[i] << " ,";
 
+        }
+        cout << "}\n\n";
+    }
+    {
+        int sum = array[0];
+        cout << "result: \n{";
+
+            for (int i = 1; i < 10; i++)
+            {
+                cout << sum << " ,";
+                sum = sum + array[i];
+            }
+
+        cout << "}\n\n";
+            
+    }
+    system("pause");
 }
-
